ac34c78810cb87af5624ae903bca97a16e0fc8df
ishaan3 <ishaan@edugorilla.org>
2020-02-10 15:50:42 +0530

    Update chart-hex-demo.js

--- NEW-UI/js/demo/chart-hex-demo.js
+++ NEW-UI/js/demo/chart-hex-demo.js
@@ -6,15 +6,15 @@ Chart.defaults.global.defaultFontColor = '#858796';
 var marksCanvas = document.getElementById("marksChart");
 
 var marksData = {
-    labels: ["English", "Maths", "Physics", "Chemistry", "Biology", "History"],
+    labels: ["Apt", "Rsn", "Eng", "Phy", "Math"],
     datasets: [{
         label: "Student A",
         backgroundColor: "rgba(200,0,0,0.2)",
-        data: [65, 75, 70, 80, 60, 80]
+        data: [65, 75, 70, 80, 69]
     }, {
         label: "Student B",
         backgroundColor: "rgba(0,0,200,0.2)",
-        data: [54, 65, 60, 70, 70, 75]
+        data: [54, 65, 60, 70, 53]
     }]
 };
 
@@ -35,8 +35,52 @@ var radarChart = new Chart(marksCanvas, {
             caretPadding: 10,
         },
         legend: {
-            display: true
+            display: false
         },
         cutoutPercentage: 100,
     },
 });
+
+
+// Set new default font family and font color to mimic Bootstrap's default styling
+// Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+// Chart.defaults.global.defaultFontColor = '#858796';
+
+// // Pie Chart Example
+// var marksCanvas = document.getElementById("marksChart");
+
+// var marksData = {
+//     labels: ["English", "Maths", "Physics", "Chemistry", "Biology", "History"],
+//     datasets: [{
+//         label: "Student A",
+//         backgroundColor: "rgba(200,0,0,0.2)",
+//         data: [65, 75, 70, 80, 60, 80]
+//     }, {
+//         label: "Student B",
+//         backgroundColor: "rgba(0,0,200,0.2)",
+//         data: [54, 65, 60, 70, 70, 75]
+//     }]
+// };
+
+
+// var radarChart = new Chart(marksCanvas, {
+//     type: 'radar',
+//     data: marksData,
+//     options: {
+//         maintainAspectRatio: false,
+//         tooltips: {
+//             backgroundColor: "rgb(255,255,255)",
+//             bodyFontColor: "#858796",
+//             borderColor: '#dddfeb',
+//             borderWidth: 1,
+//             xPadding: 15,
+//             yPadding: 15,
+//             displayColors: false,
+//             caretPadding: 10,
+//         },
+//         legend: {
+//             display: true
+//         },
+//         cutoutPercentage: 100,
+//     },
+// });
