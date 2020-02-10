a504dd9081c5f042dcf37f8068a815fa8c629397
ishaan3 <ishaan@edugorilla.org>
2020-02-10 16:34:44 +0530

    new table row

--- NEW-UI/js/demo/chart-hex-demo.js
+++ NEW-UI/js/demo/chart-hex-demo.js
@@ -6,15 +6,15 @@ Chart.defaults.global.defaultFontColor = '#858796';
 var marksCanvas = document.getElementById("marksChart");
 
 var marksData = {
-    labels: ["Apt", "Rsn", "Eng", "Phy", "Math"],
+    labels: ["Score", "Accuracy", "Correct", "Wrong", "Time"],
     datasets: [{
         label: "Student A",
         backgroundColor: "rgba(200,0,0,0.2)",
-        data: [65, 75, 70, 80, 69]
+        data: [65, 80, 55, 15, 100]
     }, {
         label: "Student B",
         backgroundColor: "rgba(0,0,200,0.2)",
-        data: [54, 65, 60, 70, 53]
+        data: [82, 90, 70, 12, 110]
     }]
 };
 
@@ -23,6 +23,13 @@ var radarChart = new Chart(marksCanvas, {
     type: 'radar',
     data: marksData,
     options: {
+        scale: {
+            ticks: {
+                beginAtZero: true,
+                min: 0,
+                max: 100
+            }
+        },
         maintainAspectRatio: false,
         tooltips: {
             backgroundColor: "rgb(255,255,255)",
